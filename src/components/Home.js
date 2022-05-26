42e2eb7cecba3c64c35b5634b6b34282ba5d35d4
yashpatel1902 <kachhiyapatel1902@gmail.com>
2022-05-25 22:50:16 -0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,100 @@
+import React,{useEffect, useState} from 'react'
+import Card from './Card';
+
+
+function Home() {
+
+    const [users, setUsers] = useState([]);
+    const [totalUsers, setTotalUsers] = useState([]);
+
+    const fetchData = ()=>{
+        fetch("https://api.hatchways.io/assessment/students")
+        .then((data) => data.json())
+        .then((data) => {
+            data = data.students
+            data.forEach((student) => {
+                if (!student.tags) {
+                    Object.assign(student, { tags: [] });
+                }
+            });
+            setUsers(data)
+            setTotalUsers(data)
+            console.log(data);
+        });
+    }
+
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    
+    const handleInputFilter = (e)=>{
+        if(e.target.value == null){
+            setUsers(totalUsers);
+        }else{
+            const filtered = totalUsers.filter(
+                item => item.firstName.toLowerCase().includes(e.target.value.toLowerCase()) ||
+                item.lastName.toLowerCase().includes(e.target.value.toLowerCase()) || 
+                (item.firstName +" "+item.lastName).toLowerCase().includes(e.target.value.toLowerCase()) )
+            setUsers(filtered)
+        }
+    }
+
+    const handleTagFilter = (e)=>{
+        if(e.target.value == null){
+            setUsers(totalUsers);
+        }else{
+            const filtered = totalUsers.filter( (user)=>{
+                let tagAdded = false;
+                user.tags.forEach(
+                    (tag)=>{ if(e.target.value.toLowerCase().includes(tag.toLowerCase())){
+                        tagAdded = true;
+                    }
+                  }
+                )
+                return tagAdded;
+            }
+            )
+            setUsers(filtered);
+        }
+    }
+
+
+
+    return (
+
+        <div className="d-flex justify-content-center">
+    
+        <div className="card scollable-card">
+
+        <div >
+            <input type="text" className='input-field' 
+            placeholder='search by name' width="500px" 
+            onInput={(e)=>handleInputFilter(e)}
+            />
+        </div>
+
+        <div >
+            <input type="text" className='input-field' 
+            placeholder='search by tag' width="500px" 
+            onInput={(e)=>handleTagFilter(e)}
+            />    
+        </div>
+
+          { users.map((currentUser)=>{
+            return(
+                <div key={currentUser.id}>
+                 <Card currentUser={currentUser} />
+                </div>
+            )
+          })
+          }
+          
+        </div>
+    
+        </div>
+     
+      );
+}
+
+export default Home
